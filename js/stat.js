92e79032d6ad6daa7a3849b2423ffc368803c4c9
Alexander Matroskin <cqb.matroskin@gmail.com>
2017-01-26 00:48:45 +0300

    [module2-task1] Оптимизация

--- js/stat.js
+++ js/stat.js
@@ -1,10 +1,28 @@
 'use strict';
+
+var colors = {
+  BLACK: '#000',
+  RED: '#f00',
+  WHITE: '#fff',
+  BLACK_ALFA: 'rgba(0, 0, 0, 0.7)'
+};
+var POINTS = [{x: 100, y: 10}, {x: 520, y: 10}, {x: 520, y: 280}, {x: 100, y: 280}];
+var COLOR_SHADOW = colors.BLACK_ALFA;
+var COLOR_WINDOW = colors.WHITE;
+var COLOR_TEXT = colors.BLACK;
+var PLAYER_COLOR = colors.RED;
+var PLAYER = 'Вы';
+var FONT = '16px PT Mono';
+var SHIFT_SHADOW = 10;
+var HISTO_HEIGHT = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_INDENT = 50;
+var PADDING = 30;
+var LINE_HEIGHT = 20;
+var WINNER_MESSAGE;
+
 // Функция отрисовки окна сообщения
 function drawWindowMessage(ctx, arrayOfPoints) {
-  var SHIFT_SHADOW = 10;
-  var COLOR_SHADOW = 'rgba(0, 0, 0, 0.7)';
-  var COLOR_WINDOW = '#fff';
-
   ctx.shadowColor = COLOR_SHADOW;
   ctx.shadowOffsetX = SHIFT_SHADOW;
   ctx.shadowOffsetY = SHIFT_SHADOW;
@@ -18,87 +36,64 @@ function drawWindowMessage(ctx, arrayOfPoints) {
   }
   ctx.closePath();
   ctx.fill();
-}
-
-function drawMessage(ctx, times, names, POINTS, PADDING, LINE_HEIGHT) {
-  var COLOR_TEXT = '#000';
-  var FONT = '16px PT Mono';
-  var min = times[0];
-  var PLAYER = 'Вы';
-  var winner;
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
-  ctx.fillStyle = COLOR_TEXT;
-  ctx.font = FONT;
+}
+
+function getMinTime(times, names) {
+  var min = times[0];
+  var winner;
   for (var i = 0; i < times.length; i++) {
     if (times[i] <= min) {
       min = times[i];
       winner = names[i];
     }
   }
-  if ( winner === PLAYER) {
-    ctx.fillText('Ура, ' + PLAYER + ' победили!', POINTS[0].x + PADDING, POINTS[0].y + PADDING);
-  } else {
-    ctx.fillText('Ура, игрок ' + winner + ' победил!', POINTS[0].x + PADDING, POINTS[0].y + PADDING);
-  }
-  ctx.fillText('Список результатов:', POINTS[0].x + PADDING, POINTS[0].y + PADDING + LINE_HEIGHT);
   return winner;
 }
 
+function drawMessage(ctx, times, names, arrayOfPoints, padding, lineHeight) {
+  var winner = getMinTime(times, names);
+  ctx.fillStyle = COLOR_TEXT;
+  ctx.font = FONT;
+  WINNER_MESSAGE = (winner === PLAYER) ? 'Ура, ' + PLAYER + ' победили!' : 'Ура, игрок ' + winner + ' победил!';
+  ctx.fillText(WINNER_MESSAGE, arrayOfPoints[0].x + padding, arrayOfPoints[0].y + padding);
+  ctx.fillText('Список результатов:', arrayOfPoints[0].x + padding, arrayOfPoints[0].y + padding + lineHeight);
+}
+// Функция возвращает максимальный элемент массива
 function calculationMaxTime(times) {
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
-  return max;
+  return Math.max.apply(null, times);
 }
 
 function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function paintigColumn(ctx, name) {
-  var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
-  var columnColor;
+function paintColumn(name) {
   var otherPlayerColor = 'rgba(0, 0, 255, ' + getRandomNumber(0.1, 1) + ')';
-  if (name === 'Вы') {
-    columnColor = PLAYER_COLOR;
-  } else {
-    columnColor = otherPlayerColor;
-  }
-  return columnColor;
+  return (name === PLAYER) ? PLAYER_COLOR : otherPlayerColor;
 }
 
-function drawColumn(ctx, POINTS, LINE_HEIGHT, max, times, names, padding) {
-  var HISTO_HEIGHT = 150;
-  var COLUMN_WIDTH = 40;
-  var COLUMN_INDENT = 50;
-  var columnX = POINTS[0].x + padding;
+function drawColumns(ctx, arrayOfPoints, lineHeight, max, times, names, padding) {
+  var columnX = arrayOfPoints[0].x + padding;
   var columnY = 250;
-  var STEP = HISTO_HEIGHT / max;
+  var step = HISTO_HEIGHT / max;
   for (var i = 0; i < times.length; i++) {
     var playerTime = times[i];
-    var height = -STEP * playerTime;
-    ctx.fillStyle = paintigColumn(ctx, names[i]);
+    var height = -step * playerTime;
+    ctx.fillStyle = paintColumn(names[i]);
     // Отрисовка колонок: columnX - стартовая точка отрисовки колонки
     // сумму отступа между колонками и ширину колонок умножаем на индекс элемента массива
     // к результату прибавляем стартовую точку
     ctx.fillRect(columnX + (COLUMN_INDENT + COLUMN_WIDTH) * i, columnY, COLUMN_WIDTH, height);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], columnX + (COLUMN_INDENT + COLUMN_WIDTH) * i, columnY + LINE_HEIGHT, COLUMN_WIDTH);
+    ctx.fillStyle = colors.BLACK;
+    ctx.fillText(names[i], columnX + (COLUMN_INDENT + COLUMN_WIDTH) * i, columnY + lineHeight, COLUMN_WIDTH);
     ctx.fillText(parseInt(playerTime, 10), columnX + (COLUMN_INDENT + COLUMN_WIDTH) * i, columnY - 10 - -height, COLUMN_WIDTH);
   }
 }
 
 window.renderStatistics = function (ctx, names, times) {
-  var POINTS = [{x: 100, y: 10}, {x: 520, y: 10}, {x: 520, y: 280}, {x: 100, y: 280}];
-  var PADDING = 30;
-  var LINE_HEIGHT = 20;
   drawWindowMessage(ctx, POINTS);
   drawMessage(ctx, times, names, POINTS, PADDING, LINE_HEIGHT);
-  drawColumn(ctx, POINTS, LINE_HEIGHT, calculationMaxTime(times), times, names, PADDING);
+  drawColumns(ctx, POINTS, LINE_HEIGHT, calculationMaxTime(times), times, names, PADDING);
 };
-
