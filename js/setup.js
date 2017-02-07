67bdc823e383163a733ab3372e458b7c2322e225
Alexander Matroskin <cqb.matroskin@gmail.com>
2017-02-07 23:28:01 +0300

    [module4-task1] Коммит для ревью

--- js/setup.js
+++ js/setup.js
@@ -8,6 +8,7 @@ var setupWizardElement = setupWindowElement.querySelector('.setup-wizard');
 var wizardCoatElement = setupWizardElement.querySelector('#wizard-coat');
 var wizardEyesElement = setupWizardElement.querySelector('#wizard-eyes');
 var wizarFireBallElement = setupWindowElement.querySelector('.setup-fireball-wrap');
+var submitBtnElement = setupWindowElement.querySelector('.button');
 var currentCoatColorIndex = 0;
 
 /**
@@ -19,26 +20,99 @@ var Сolors = {
   FOR_FIRE_BALL: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
 };
 
-/** открывает окно настроек */
-function openSetupWindow() {
+/**
+ * @enum {number}
+ */
+var KeyCodeLib = {
+  ESCAPE: 27,
+  ENTER: 13
+};
+
+var TitleConfig = {
+  ZERO: 0,
+  MAX_LENGTH: 50
+};
+
+/**
+ * проверяет совпадает ли evt.keyCode и клавиша переданная вторым аргументом
+ * @param {KeyboardEvent} evt
+ * @param {number} key
+ * @return {boolean}
+ */
+
+/** открывает окно настроек навешивает слушатели */
+function openSetupWindowHandler() {
   setupWindowElement.classList.remove('invisible');
-  wizardCoatElement.addEventListener('click', paintWizardCoat);
-  wizardEyesElement.addEventListener('click', paintWizardEyes);
-  wizarFireBallElement.addEventListener('click', paintFireBall);
+
+  wizardCoatElement.addEventListener('click', paintWizardCoatHandler);
+  wizardEyesElement.addEventListener('click', paintWizardEyesHandler);
+  wizarFireBallElement.addEventListener('click', paintFireBallHandler);
+  submitBtnElement.addEventListener('click', submitBtnHandler);
+  submitBtnElement.addEventListener('click', pressSubmitToClose);
+  document.addEventListener('keydown', pressEscToCloseSetupWindow);
 }
 
-/** закрывает окно настроек */
-function closeSetupWindow() {
+/** закрывает окно настроек удаляет слушатели */
+function closeSetupWindowHandler() {
   setupWindowElement.classList.add('invisible');
-  wizardCoatElement.removeEventListener('click', paintWizardCoat);
-  wizardEyesElement.removeEventListener('click', paintWizardEyes);
-  wizarFireBallElement.removeEventListener('click', paintFireBall);
+
+  wizardCoatElement.removeEventListener('click', paintWizardCoatHandler);
+  wizardEyesElement.removeEventListener('click', paintWizardEyesHandler);
+  wizarFireBallElement.removeEventListener('click', paintFireBallHandler);
+  submitBtnElement.removeEventListener('click', submitBtnHandler);
+  submitBtnElement.removeEventListener('click', pressSubmitToClose);
+  document.removeEventListener('keydown', pressEscToCloseSetupWindow);
+}
+
+function isActivateEvent(evt, key) {
+  return evt.keyCode && evt.keyCode === key;
+}
+
+function keyPressOpenSetupWindow(evt) {
+  if (isActivateEvent(evt, KeyCodeLib.ENTER)) {
+    openSetupWindowHandler();
+  }
+}
+
+function keyPressCloseSetupWindow(evt) {
+  if (isActivateEvent(evt, KeyCodeLib.ENTER)) {
+    closeSetupWindowHandler();
+  }
+}
+
+function pressSubmitToClose(evt) {
+  if (isActivateEvent(evt, KeyCodeLib.ESCAPE) && validateName()) {
+    closeSetupWindowHandler();
+  }
+}
+
+function pressEscToCloseSetupWindow(evt) {
+  if (isActivateEvent(evt, KeyCodeLib.ESCAPE)) {
+    closeSetupWindowHandler();
+  }
 }
 
 /** устанавливает атрибуты для поля Имени */
 function setNameValidation() {
   inputNameElement.required = true;
   inputNameElement.maxLength = 50;
+}
+
+/**
+ * проверяет валидность поля имени
+ * @return {boolean}
+ */
+function validateName() {
+  if (inputNameElement.length > TitleConfig.ZERO && inputNameElement.length < TitleConfig.MAX_LENGTH) {
+    return true;
+  }
+  return false;
+}
+
+function submitBtnHandler(evt) {
+  if (validateName()) {
+    closeSetupWindowHandler();
+  }
 }
 
 /**
@@ -51,21 +125,23 @@ function returnRandomIndex(array) {
 }
 
 /** выводим мантии по порядку */
-function paintWizardCoat() {
+function paintWizardCoatHandler() {
   currentCoatColorIndex = (currentCoatColorIndex + 1) % Сolors.FOR_COAT.length;
   wizardCoatElement.style.fill = Сolors.FOR_COAT[currentCoatColorIndex];
 }
 
 /** рандомно переключает цвет глаз */
-function paintWizardEyes() {
+function paintWizardEyesHandler() {
   wizardEyesElement.style.fill = Сolors.FOR_EYES[returnRandomIndex(Сolors.FOR_EYES)];
 }
 
 /** рандомно переключает цвет фаерболла */
-function paintFireBall() {
+function paintFireBallHandler() {
   wizarFireBallElement.style.background = Сolors.FOR_FIRE_BALL[returnRandomIndex(Сolors.FOR_EYES)];
 }
 
 setNameValidation();
-setupOpenElement.addEventListener('click', openSetupWindow);
-setupCloseElement.addEventListener('click', closeSetupWindow);
+setupOpenElement.addEventListener('click', openSetupWindowHandler);
+setupOpenElement.addEventListener('keydown', keyPressOpenSetupWindow);
+setupCloseElement.addEventListener('click', closeSetupWindowHandler);
+setupCloseElement.addEventListener('keydown', keyPressCloseSetupWindow);
